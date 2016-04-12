7a803a90c4788e3ba514454e4fb2f99d33ee1c74
Mohammed Ismail <mohammed.ismail@apptizer.io>
2016-04-12 13:55:58 +0530

    Initial commit

---
+++ PhoneNumberRnD/app/src/main/java/com/test/phonenumberrnd/MainActivity.java
@@ -0,0 +1,26 @@
+package com.test.phonenumberrnd;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+    }
+
+
+    public void onClick(View view) {
+        TelephonyManager tMgr = (TelephonyManager)this.getSystemService(Context.TELEPHONY_SERVICE);
+        String mPhoneNumber = tMgr.getLine1Number();
+        editText.setText(mPhoneNumber);
+    }
+}
